5c47ba7d10d7a1a725f57ca5ab93940f026c5b7b
Majistor <ashirwadp23@iitk.ac.in>
2024-09-05 23:40:53 +0530

    ui

--- lib/pages/add_transaction.dart
+++ lib/pages/add_transaction.dart
@@ -1,33 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:optiparse/common/color_extension.dart';
 
 class AddTransaction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: TColor.gray,
       appBar: AppBar(
-        title: Text('Transaction App Home'),
+        backgroundColor: TColor.gray,
+        title: Text(
+          'Add Transaction',
+          style: TextStyle(color: TColor.white),
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: TColor.gray30),
               onPressed: () {
                 Navigator.pushNamed(context, 'manual_transaction');
               },
-              child: Text('Add Manual Transaction'),
+              child: Text(
+                'Add Manual Transaction',
+                style: TextStyle(color: TColor.white),
+              ),
             ),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: TColor.gray30),
               onPressed: () {
                 Navigator.pushNamed(context, 'file_picker');
               },
-              child: Text('Select Files'),
+              child: Text(
+                'Select Files',
+                style: TextStyle(color: TColor.white),
+              ),
             ),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: TColor.gray30),
               onPressed: () {
                 Navigator.pushNamed(context, 'image_picker');
               },
-              child: Text('Capture Images'),
+              child: Text(
+                'Capture Images',
+                style: TextStyle(color: TColor.white),
+              ),
             ),
           ],
         ),
